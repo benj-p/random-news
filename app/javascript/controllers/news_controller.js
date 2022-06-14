5c63964f20a3fbb0731d28539c109e7038ee1f03
Ben Pham <benjamin.pham@gmail.com>
2022-06-14 21:31:28 +0200

    GetNews API

--- app/javascript/controllers/news_controller.js
+++ app/javascript/controllers/news_controller.js
@@ -1,9 +1,31 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
+import axios from 'axios';
 
 export default class extends Controller {
-  static targets = ["newsContainer", "readMoreButton", "refreshButton"]
-
+  static targets = [ "newsContainer", "readMoreButton" ]
+  
   refresh(event) {
-    console.log("Hello from news controller again");
+    event.preventDefault()
+    this.getNews();
+  }
+
+  getNews() {
+    axios.get('https://newsapi.org/v2/everything?language=en&apiKey=a59c003a1c3f4c67b2c49fbdc5b492ea&q=barcelona&searchIn=title')
+      .then(function (response) {
+        const articles = response["data"]["articles"]
+        const article = articles[articles.length * Math.random() | 0] 
+        return article
+      })
+      .then((response) => {
+        this.newsContainerTarget.innerHTML = response["description"]
+        this.readMoreButtonTarget.href = response["url"]
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+    }
+    
+  connect() {
+    this.getNews();
   }
 }
