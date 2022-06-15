eb978740d3d42fe916bc8f3c4bc0a045f921deae
Ben Pham <benjamin.pham@gmail.com>
2022-06-15 12:26:20 +0200

    Linked Article to stimulus NewsController

--- app/javascript/controllers/news_controller.js
+++ app/javascript/controllers/news_controller.js
@@ -1,31 +1,26 @@
 import { Controller } from "@hotwired/stimulus";
-import axios from 'axios';
 
 export default class extends Controller {
-  static targets = [ "newsContainer", "readMoreButton" ]
+  static targets = [ "newsContainer", "readMoreButton", "articleTitle", "articleBody" ]
   
   refresh(event) {
     event.preventDefault()
-    this.getNews();
+    this.getArticle();
   }
 
-  getNews() {
-    axios.get('https://newsapi.org/v2/everything?language=en&apiKey=a59c003a1c3f4c67b2c49fbdc5b492ea&q=barcelona&searchIn=title')
-      .then(function (response) {
-        const articles = response["data"]["articles"]
-        const article = articles[articles.length * Math.random() | 0] 
-        return article
-      })
-      .then((response) => {
-        this.newsContainerTarget.innerHTML = response["description"]
-        this.readMoreButtonTarget.href = response["url"]
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
-    }
-    
-  connect() {
-    this.getNews();
+  updateContent(article) {
+    this.articleTitleTarget.innerHTML = article["title"]
+    this.articleBodyTarget.innerHTML = article["short_body"]
+    this.newsContainerTarget.dataset.id = article["id"]
+    this.readMoreButtonTarget.href = article["url"]
+  }
+
+  getArticle() {
+    fetch('/articles')
+    .then((response) => response.json())
+    .then((data) => {
+        const article = data["articles"][data["articles"].length * Math.random() | 0]
+        this.updateContent(article)
+    });
   }
 }
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
   validates :source, :url, :title, :body, :article_source_id, presence: true
   validates :article_source_id, uniqueness: { scope: :source, message: "article already exists" }
+
+  def short_body
+    body.truncate(2000, separator: /<\/p>/)
+  end
 end
