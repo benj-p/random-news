ea1bf97a34ef125142a64e9bf28ef5a2f16a258c
Ben Pham <benjamin.pham@gmail.com>
2022-06-16 08:52:58 +0200

    Fixed article from last 24h

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @article = Article.where('published_date < ?', 24.hours.ago).order(published_date: :desc).first
+    @article = Article.where('published_date > ?', 24.hours.ago).order(published_date: :desc).first
   end
 end
