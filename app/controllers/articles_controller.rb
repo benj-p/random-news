54e1e2e1e5668ff46868d4c40c186e12a0febca6
Ben Pham <benjamin.pham@gmail.com>
2022-06-17 08:42:10 +0200

    History tracking

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,9 +3,11 @@ class ArticlesController < ApplicationController
   
   def index
     age_hours = params[:age_hours] || nil
+    history = params[:history] || nil
     
     articles = Article.all
     articles = Article.where('published_date > ?', age_hours.to_i.hours.ago) if age_hours
+    articles = articles.where.not(id: params[:history]) if history
 
     render json: { articles: articles.as_json(methods: :short_body) } 
   end
--- app/javascript/controllers/news_controller.js
+++ app/javascript/controllers/news_controller.js
@@ -1,18 +1,57 @@
+const localSessionKey = "history.history"
+
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = [ "newsContainer", "readMoreButton", "articleTitle", "articleBody", "loader" ]
+  static targets = [ "newsContainer", "readMoreButton", "refreshButton", "articleTitle", "articleBody", "loader", "allRead" ]
   
-  refresh(event) {
+  async refresh(event) {
     event.preventDefault()
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: 'smooth' })
     this.loaderTarget.classList.remove('inactive')
     this.articleTitleTarget.innerHTML = null
     this.articleBodyTarget.innerHTML = null  
-    const timerDuration = Math.floor((Math.random() * 1000) + 200);
-    setTimeout(() => {  
-      this.getArticle();
-      this.loaderTarget.classList.add('inactive') }, timerDuration);
+
+    let searchParams = this.setSearchParams()
+    const data = await fetch(`/articles?${searchParams}`).then((response) => response.json())
+    const timerDuration = Math.floor((Math.random() * 1000) + 200)
+    
+    if (data["articles"].length === 0) {
+      setTimeout(() => {
+        this.loaderTarget.classList.add('inactive')
+        this.allReadTarget.classList.remove('inactive')
+        this.readMoreButtonTarget.style.pointerEvents = "none";
+        this.refreshButtonTarget.style.pointerEvents = "none";
+      }, timerDuration);      
+    } else {
+      let article = data["articles"][data["articles"].length * Math.random() | 0]
+      this.updateHistory(article["id"])
+      setTimeout(() => {
+        this.loaderTarget.classList.add('inactive')
+        this.updateContent(article)
+      }, timerDuration);
+    }
+
+  }
+
+  setSearchParams() {
+    let searchParams = new URLSearchParams()
+    searchParams.set("age_hours", 12)
+    this.history.forEach((id) => {
+      searchParams.append("history[]", id)
+    });
+    return searchParams.toString()
+  }
+
+  setHistory() {
+    let historyValue = window.sessionStorage.getItem(localSessionKey)
+    this.history = JSON.parse(historyValue)
+  }
+
+  updateHistory(id) {
+    this.setHistory()
+    this.history.push(id)
+    window.sessionStorage.setItem(localSessionKey, JSON.stringify(this.history))
   }
 
   updateContent(article) {
@@ -22,12 +61,8 @@ export default class extends Controller {
     this.readMoreButtonTarget.href = article["url"]
   }
 
-  getArticle() {
-    fetch('/articles?age_hours=24')
-    .then((response) => response.json())
-    .then((data) => {
-        const article = data["articles"][data["articles"].length * Math.random() | 0]
-        this.updateContent(article)
-    });
+  connect() {
+    this.history = [parseInt(this.newsContainerTarget.dataset.id)]
+    window.sessionStorage.setItem(localSessionKey, JSON.stringify(this.history))
   }
 }
